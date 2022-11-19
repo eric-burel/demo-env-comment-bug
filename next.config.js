0e178b25e1f947d7e31af89c0d3042ecd3770fc2
eric-burel <eb@lbke.fr>
2022-11-19 18:12:36 +0100

    show more scenarios

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 console.log("process.env.FOO", process.env.FOO);
+console.log("process.env.QUOTES", process.env.QUOTES);
+console.log("process.env.NO_QUOTES", process.env.NO_QUOTES);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
